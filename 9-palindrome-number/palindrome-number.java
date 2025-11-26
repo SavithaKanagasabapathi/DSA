c781a3f7ac1ac5b542471681e6c4d8e1ad17b553
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2025-11-25 17:05:13 -0800

    Time: 5 ms (69.63%) | Memory: 45.7 MB (33.66%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverseX = 0;
+        int temp = x;
+        while (temp > 0) {
+            reverseX = reverseX * 10 + (temp % 10);
+            temp = temp / 10;
+        }
+        return x == reverseX;
+    }
+}
