935adaa54d9247e0e64aeb58af6fa50b96942375
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-01-16 21:24:24 -0800

    Time: 2 ms (98.83%) | Memory: 44.2 MB (80.06%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //TC-O(n) and SC-O(1)
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {//Ignore non alphanumerical
+                left++;
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            } else {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
