489698d8498d1020721201b419ee0504d1dae728
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-01-23 16:04:03 -0800

    Time: 1 ms (72.79%) | Memory: 43.5 MB (76.48%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        //TC-O(N) and SC-O(H), H is height of recursion stack
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        List<Integer> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = deque.poll();
+                if (i == size - 1) {
+                    result.add(node.val);//Add last element
+                }
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+            }
+        }
+        return result;
+    }
+}
