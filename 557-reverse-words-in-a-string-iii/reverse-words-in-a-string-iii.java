b9145f6134ffc46649c2812082a9da72e1d88bdf
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-04-01 17:08:03 -0700

    Time: 6 ms (48.60%) | Memory: 47 MB (50.23%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -2,12 +2,18 @@ class Solution {
     public String reverseWords(String s) {
         //TC-O(N) and SC-O(N)
 
-        List<String> splittedStrings = Arrays.asList(s.split(" "));
-        StringBuilder result = new StringBuilder();
-        splittedStrings.forEach(splits -> {
-            result.append(new StringBuilder(splits).reverse());
-            result.append(" ");
-        });
-        return result.toString().trim();
+        // List<String> splittedStrings = Arrays.asList(s.split(" "));
+        // StringBuilder result = new StringBuilder();
+        // splittedStrings.forEach(splits -> {
+        //     result.append(new StringBuilder(splits).reverse());
+        //     result.append(" ");
+        // });
+        // return result.toString().trim();
+
+        return Arrays.stream(s.split(" "))
+                .map(word -> new StringBuilder(word).reverse().toString())
+                .collect(Collectors.joining(" "));
+
+        //Strng.join() and .collect() have delimiters, so they don't add trail and ;ead spaces
     }
 }
