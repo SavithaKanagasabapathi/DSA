c53794a225283c2e20be0a9a8a79c4e3d3b33283
Savitha Kanagasabapathi <k.savitha1997@gmail.com>
2026-04-29 13:49:40 -0700

    Time: 0 ms (100.00%) | Memory: 43.7 MB (64.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,20 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //TC-O(logn) and SC-O(1)
-        //Rule: Uphill values be greater than index 0 and downhill will be lesser than index 0
+        //TC-O(log n) and SC-O(1)
+        //if target given, return in mid, so left<=right and return default outside
+        //already mid and target checked, so in later loops with mid and target don't include =
+        //else in other places, use =
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] >= nums[0]) {
-                if (target >= nums[0] && target < nums[mid]) {
+            } else if (nums[left] <= nums[mid]) {//left sorted
+                if (target < nums[mid] && target >= nums[left]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
-                if (target <= nums[right] && target > nums[mid]) {
+            } else {//right sorted
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
